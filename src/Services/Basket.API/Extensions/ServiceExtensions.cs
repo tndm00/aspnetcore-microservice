271c8e84981e11c3415433bf827e98f6be2f5d51
DESKTOP-2B8P54H\nhant <tthnhanit@gmail.com>
2022-11-26 13:38:56 +0700

    [4.3]-[4.4] Design Pattern - Connect with Redis

---
+++ src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Contracts.Common.Interfaces;
+using Infrastructure.Common;
+
+namespace Basket.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services) =>
+            services.AddScoped<IBasketRepository, BasketRepository>()
+            .AddTransient<ISerializeService, SerializeService>();
+
+        public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisConnectionSting = configuration.GetSection("CacheSettings:ConnectionString").Value;
+            if (string.IsNullOrEmpty(redisConnectionSting))
+            {
+                throw new ArgumentNullException("Redis Connection string is not configured");
+            }
+
+            //Redis Configuration
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = redisConnectionSting;
+            });
+        }
+    }
+}
